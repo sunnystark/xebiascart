8f3113509594f2546f25083ca3483db4259b655f
sunnystark <sunnystark19@gmail.com>
2020-08-20 01:17:55 +0530

    making discount component

---
+++ src/components/discountCard/DiscountCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PriceSelection from './PriceSelectionComponent'
+import styles from './PriceCard.module.css'
+
+function PriceCard(props) {
+  const selectOption = {key:-1, displayValue:'select'}
+  const priceRangeClone = [...props.values]
+  const minValues = [selectOption, ...priceRangeClone.slice(0, priceRangeClone.length - 1)];
+  const maxValues = [selectOption, ...priceRangeClone.slice(1)];
+  return (
+    <div className={styles.container}>
+      <h3>Price</h3>
+      <br/>
+      <hr/>
+      <div className={styles.options}>
+        <div style={{display:'inline-block', marginRight:'1rem'}}>
+          <span>min: </span>
+          <PriceSelection 
+            values={minValues} 
+            handleChange={props.handleMinChange} 
+            selectedValue={props.selectedMinMax[0]} 
+          />
+        </div>
+        <div style={{display:'inline-block'}}>
+          <span>max: </span>
+          <PriceSelection 
+            values={maxValues} 
+            handleChange={props.handleMaxChange}
+            selectedValue={props.selectedMinMax[1]} 
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PriceCard;
